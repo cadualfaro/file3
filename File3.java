2146aba68cbf17040bce27d3ddc76f37bb41820c
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 18:33:48 -0300

    Update File3.java

--- File3.java
+++ File3.java
@@ -1,12 +1,3 @@
-/*
-Atividade 3
-
-Vamos praticar!!
-▪ Faça uma função que recebe 2 parâmetros
-▪ um path de um arquivo de texto e uma String a ser concatenada no arquivo
-▪ A função deve concatenar (adicionar no final do arquivo) a String passada
-▪ Dica: você terá que usar FileWriter(filename,true)
- */
 package file3;
 
 import java.io.*;
